64b6ab28fcb23527b3b16922893a1aaf0c9e661f
tatsufinn-commits <tatsufinn@gmail.com>
2026-08-12 05:03:52 +0800

    Delete files directory

--- files/src/data/affinities.js
+++
@@ -1,5 +0,0 @@
-// Build 6: explicit neutral defaults. Add only lore-approved interactions here.
-export const affinityTable = Object.freeze({
-  Water: Object.freeze({}), Fire: Object.freeze({}), Wind: Object.freeze({}), Earth: Object.freeze({}),
-  Healing: Object.freeze({}), Barrier: Object.freeze({}), Neutral: Object.freeze({})
-});
